c8eecb9ca67b20e2baca3d7885c16e5fc3050911
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-09-24 10:35:07 +0530

    Building Routs using React Router Dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import PrivateRoute from "./components/PrivateRoute";
+import Header from "./components/Header";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/listing/:listingId" element={<Listing />} />
+          <Route path="/search" element={<Search />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+            <Route path="/updat-listing" element={<UpdateListing />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
