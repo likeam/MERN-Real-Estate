badadce30daa285cbbbb55c31f4e2a09d441e62d
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-09-24 16:25:10 +0530

    Building Router and Controller

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/userRoutes.js";
 
 mongoose
   .connect("mongodb+srv://likeam99:kakakolo202@cluster0.f3cql.mongodb.net/")
@@ -11,3 +12,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Srerver is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
