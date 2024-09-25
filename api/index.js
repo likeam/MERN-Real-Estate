f81e5624b239f75a2e65340063910b46eb88d060
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-09-25 11:41:43 +0530

    Building SignUp API Authentication

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRouter from "./routes/userRoutes.js";
+import userRouter from "./routes/userRoute.js";
+
+import authRouter from "./routes/authRoute.js";
 
 mongoose
   .connect("mongodb+srv://likeam99:kakakolo202@cluster0.f3cql.mongodb.net/")
@@ -8,9 +10,21 @@ mongoose
   .catch((error) => console.log("Error connecting", error));
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Srerver is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
