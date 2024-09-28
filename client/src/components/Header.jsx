e315a7a32787b6a49496ea68e1d36752a6a214db
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-09-29 00:14:25 +0500

    Creating avatar photo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
       <header className=" bg-slate-200 shadow-md">
@@ -12,7 +17,7 @@ const Header = () => {
               <span className=" text-slate-700">Estate</span>
             </h1>
           </Link>
-          <form a className=" bg-slate-100 p-3 rounded-lg flex items-center">
+          <form className=" bg-slate-100 p-3 rounded-lg flex items-center">
             <input
               type="text"
               placeholder="Search..."
@@ -33,11 +38,22 @@ const Header = () => {
                 About
               </li>
             </Link>
-            <Link to="/sign-in">
+            <Link to="/profile">
+              {currentUser ? (
+                <img
+                  src={currentUser.avatar}
+                  alt="avatar"
+                  className=" rounded-full h-7 object-cover"
+                />
+              ) : (
+                <li className=" text-slate-700 hover:underline ">Sign in</li>
+              )}
+            </Link>
+            {/* <Link to="/sign-in">
               <li className=" hidden sm:inline text-slate-700 hover:underline">
                 Sign In
               </li>
-            </Link>
+            </Link> */}
           </ul>
         </div>
       </header>
