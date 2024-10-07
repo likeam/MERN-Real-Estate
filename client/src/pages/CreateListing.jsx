ecf4cfccc5ea9fb8bf3e9e6efca241a43dc3ef2a
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-10-07 21:35:51 +0500

    iNTEGRATING listying with api

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+
+import { app } from "../firebase.js";
 
 const CreateListing = () => {
   const [files, setFiles] = useState([]);
@@ -29,7 +37,33 @@ const CreateListing = () => {
 
   const handleImageSubmit = (e) => {
     e.preventDefault();
-    setUploading(true);
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((error) => {
+          setImageUploadError(
+            "Image upload faild. (2 mb max per image) ",
+            error
+          );
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError(" You can only upload 6 images per listing");
+      setUploading(false);
+    }
   };
 
   const handleChange = (e) => {
@@ -70,10 +104,63 @@ const CreateListing = () => {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError(" You must upload at least one image ");
+
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError("Discount price must be less than regular price");
+
+      setLoading(true);
+      setError(false);
+
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          useRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(error.message);
+        return;
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+      console.log(error);
+    }
   };
   const storeImage = async (file) => {
-    setLoading(true);
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done.`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
   };
 
   return (
@@ -100,9 +187,9 @@ const CreateListing = () => {
             />
             <textarea
               type="text"
-              className=" border p-3 rounded-lg"
-              placeholder="Discription"
-              id="discription"
+              placeholder="Description"
+              className="border p-3 rounded-lg"
+              id="description"
               required
               onChange={handleChange}
               value={formData.description}
@@ -110,9 +197,9 @@ const CreateListing = () => {
 
             <input
               type="text"
-              className=" border p-3 rounded-lg"
-              placeholder="Adress"
-              id="adress"
+              placeholder="Address"
+              className="border p-3 rounded-lg"
+              id="address"
               required
               onChange={handleChange}
               value={formData.address}
@@ -226,7 +313,9 @@ const CreateListing = () => {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted price</p>
-
+                  {formData.type === "rent" && (
+                    <span className=" text-xs "> (Rs / month))</span>
+                  )}
                   <span className="text-xs">/Month</span>
                 </div>
               </div>
@@ -286,7 +375,7 @@ const CreateListing = () => {
               disabled={loading || uploading}
               className=" p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-90 disabled:opacity-70"
             >
-              {loading ? "Creating ..." : "Creating Listing"}
+              {loading ? "Creating ..." : "Create"}
             </button>
             {error && <p className=" text-red-700 text-sm">{error}</p>}
           </div>
